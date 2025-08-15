7e7d166eff84904930d0df6de9f89c7fbe981f3d
Abdul Basith N <156491242+786ab-dul@users.noreply.github.com>
2025-08-15 11:16:54 +0530

    Time: 7 ms (1.38%) | Memory: 45.8 MB (8.51%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -1,18 +1,18 @@
 //Abdul's
 class Solution {
     public int[] getConcatenation(int[] nums) {
-       int n=nums.length;
+        int n = nums.length;
 
-       int[] ans=new int[2*n];
+        int[] ans = new int[2 * n];
 
-       HashMap<Integer,Integer> map= new HashMap();
+        HashMap<Integer, Integer> map = new HashMap();
 
-       for(int i=0; i<n;i++){
-        map.put(i,nums[i]);
+        for (int i = 0; i < n; i++) {
+            map.put(i, nums[i]);
 
-        ans[i]=map.get(i);
-        ans[i+n]=map.get(i);
-       } 
-       return ans;
+            ans[i] = map.get(i);
+            ans[i + n] = map.get(i);
+        }
+        return ans;
     }
 }
