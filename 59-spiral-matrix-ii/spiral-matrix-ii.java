ff6c613906fc57c0d97333e18af33b5fc5c87808
Abdul Basith N <156491242+786ab-dul@users.noreply.github.com>
2025-08-15 10:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (9.22%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -0,0 +1,37 @@
+//Abdul's
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int r1 = 0, r2 = n - 1;
+        int c1 = 0, c2 = n - 1;
+
+        int[][] arr = new int[n][n];
+        int val = 1;
+
+        while (r1 <= r2 && c1 <= c2) {
+            //left to right
+            for (int c = c1; c <= c2; c++)
+                arr[r1][c] = val++;
+
+            //top to down
+            for (int r = r1 + 1; r <= r2; r++)
+                arr[r][c2] = val++;
+
+            //to move roght to left and moving down to top we use if
+
+            if (r1 < r2 && c1 < c2) {
+                //move right to left 
+                for (int c = c2 - 1; c > c1; c--)
+                    arr[r2][c] = val++;
+
+                //move down to up
+                for (int r = r2; r > r1; r--)
+                    arr[r][c1] = val++;
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return arr;
+    }
+}
